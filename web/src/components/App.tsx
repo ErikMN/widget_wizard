549ac8816665d13213ec10da9194de3cc3fdd1b7
ErikMN <yubnut@gmail.com>
2024-08-25 16:06:35 +0200

    Add initial video player

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,10 +1,27 @@
-import '../assets/css/App.css';
+import { useEffect, useState } from 'react';
 import GetParam from './GetParam';
+import VideoPlayer from './VideoPlayer.jsx';
+import '../assets/css/App.css';
 
 function App() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenHeight, setScreenHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
       <GetParam param="Brand.ProdFullName" />
+      <VideoPlayer width={screenWidth} height={screenHeight} />
     </div>
   );
 }
---
+++ web/src/components/VideoPlayer.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react';
+import { Player } from 'media-stream-player';
+import PropTypes from 'prop-types';
+
+const OFFSET = 250;
+
+interface VapixConfig {
+  compression: string;
+  resolution: string;
+}
+
+interface VideoPlayerProps {
+  height: number;
+  width?: number;
+}
+
+/* Force a login by fetching usergroup */
+const authorize = async (): Promise<void> => {
+  try {
+    await window.fetch('/axis-cgi/usergroup.cgi', {
+      credentials: 'include',
+      mode: 'no-cors'
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+/* Set default Vapix params if not already set */
+const setDefaultParams = (): void => {
+  const existingVapixJSON = localStorage.getItem('vapix');
+  if (!existingVapixJSON) {
+    const vapixConfig: VapixConfig = {
+      compression: '20',
+      resolution: '1280x720'
+    };
+    const vapixJSON = JSON.stringify(vapixConfig);
+    localStorage.setItem('vapix', vapixJSON);
+    console.log('Setting Vapix params:', vapixJSON);
+  }
+};
+
+const Authenticating: React.FC = () => {
+  return <h3 style={{ color: 'white' }}>Authenticating...</h3>;
+};
+
+const VideoPlayer: React.FC<VideoPlayerProps> = ({ width, height }) => {
+  const [authorized, setAuthorized] = useState<boolean>(false);
+
+  let vapixParams: Partial<VapixConfig> = {};
+  try {
+    vapixParams = JSON.parse(window.localStorage.getItem('vapix') || '{}');
+  } catch (err) {
+    console.warn('No stored VAPIX parameters: ', err);
+  }
+
+  useEffect(() => {
+    authorize()
+      .then(() => {
+        setAuthorized(true);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    setDefaultParams();
+  }, []);
+
+  if (!authorized) {
+    return <Authenticating />;
+  }
+
+  return (
+    <div
+      style={{
+        height: `${height - OFFSET}px`,
+        width: `${width}px`,
+        flex: 1,
+        backgroundColor: 'black',
+        padding: '3px'
+      }}
+    >
+      <Player
+        hostname={window.location.host}
+        initialFormat="RTP_H264"
+        autoPlay
+        autoRetry
+        vapixParams={vapixParams}
+      />
+    </div>
+  );
+};
+
+VideoPlayer.propTypes = {
+  height: PropTypes.number.isRequired
+};
+
+export default VideoPlayer;
