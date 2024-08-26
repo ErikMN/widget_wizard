9f794eb78fe41c715c27d42fe0bcaf5cacabe24b
ErikMN <yubnut@gmail.com>
2024-08-26 20:02:10 +0200

    Add drawer test

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Box } from '@mui/material';
 import GetParam from './GetParam';
 import VideoPlayer from './VideoPlayer';
+/* MUI */
+import { Container, Box, Drawer, IconButton } from '@mui/material';
+import { Menu as MenuIcon, Close as CloseIcon } from '@mui/icons-material';
 import '../assets/css/App.css';
 
+const drawerWidth = 400;
+
 const App: React.FC = () => {
+  /* Local state */
   const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
   const [screenHeight, setScreenHeight] = useState<number>(window.innerHeight);
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -18,6 +24,10 @@ const App: React.FC = () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
   return (
     <Container>
@@ -31,11 +41,33 @@ const App: React.FC = () => {
           height: '100vh'
         }}
       >
-        <GetParam param="Brand.ProdFullName" />
+        <Drawer
+          variant="persistent"
+          anchor="right"
+          open={drawerOpen}
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box'
+            }
+          }}
+        >
+          HELLO DRAWER
+        </Drawer>
+
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton onClick={toggleDrawer}>
+            {drawerOpen ? <CloseIcon /> : <MenuIcon />}
+          </IconButton>
+          <GetParam param="Brand.ProdFullName" />
+        </Box>
+
         <VideoPlayer width={screenWidth} height={screenHeight} />
       </Box>
     </Container>
   );
-}
+};
 
 export default App;
