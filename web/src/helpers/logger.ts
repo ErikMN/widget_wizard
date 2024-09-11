24465df56d407b3d5772b794e944898411af6c3d
ErikMN <yubnut@gmail.com>
2024-09-11 20:19:31 +0200

    Fix logger again

--- web/src/helpers/logger.ts
+++ web/src/helpers/logger.ts
@@ -4,7 +4,7 @@ export const enableLogging = (enable: boolean) => {
   isLoggingEnabled = enable;
 };
 
-export const log = (...args: string[]) => {
+export const log = (...args: (string | object)[]) => {
   if (isLoggingEnabled) {
     console.log(...args);
   }
