9f80a4f74c5a4b0ca7993675818ea89ee4245ef2
ErikMN <yubnut@gmail.com>
2024-09-11 19:28:25 +0200

    Add overlay example from media-stream-library-js

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
 import App from './components/App';
 
+/* Comment out App and index.css above to use the overlay example */
+// import App from './components/overlay-example/App';
+
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement!);
 
