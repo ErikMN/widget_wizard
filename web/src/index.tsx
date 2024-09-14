38c0d07e3702cbbbb33d5dfdc9d3c1afc4dd47cb
ErikMN <yubnut@gmail.com>
2024-09-14 15:09:01 +0200

    Dynamic switch between WW and ovlexample

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/css/index.css';
-import App from './components/App';
-
-/* Comment out App and index.css above to use the overlay example */
-// import App from './components/overlay-example/App';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement!);
 
+const isOverlayExample = localStorage.getItem('ovlexample') === 'true';
+
+if (!isOverlayExample) {
+  /* Widget Wizard */
+  import('./assets/css/index.css');
+  import('./components/App').then(({ default: App }) => {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  });
+} else {
+  /* Overlay example */
+  import('./components/overlay-example/App').then(({ default: App }) => {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  });
+}
+/*
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
