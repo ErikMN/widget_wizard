c7e24c6067d015b51998bf85e9407b9c483a36ef
ErikMN <yubnut@gmail.com>
2024-11-24 19:02:28 +0100

    WidgetHandler: Open last created widget by default

--- web/src/components/widget/WidgetHandler.tsx
+++ web/src/components/widget/WidgetHandler.tsx
@@ -80,6 +80,18 @@ const WidgetHandler: React.FC = () => {
     }
   }, [activeDraggableWidget, activeWidgets]);
 
+  /* Effect for opening last added widget by default */
+  useEffect(() => {
+    if (activeWidgets.length > 0) {
+      const latestWidget = activeWidgets[activeWidgets.length - 1];
+      setActiveDraggableWidget((prev) => ({
+        ...prev,
+        id: latestWidget.generalParams.id
+      }));
+      setOpenDropdownIndex(activeWidgets.length - 1);
+    }
+  }, [activeWidgets, setActiveDraggableWidget, setOpenDropdownIndex]);
+
   /* Handle dropdown change */
   const handleWidgetChange = useCallback(
     (event: SelectChangeEvent<string>) => {
