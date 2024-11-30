9f30dca7632dea8fb7c4ead9e7f2005fa672124a
ErikMN <yubnut@gmail.com>
2024-11-30 18:12:23 +0100

    constants: Remove unused drawerOffset

--- web/src/components/constants.ts
+++ web/src/components/constants.ts
@@ -6,4 +6,3 @@ export const P_CGI = '/axis-cgi/param.cgi?action=list';
 /* Drawer width and auto-close offset */
 export const drawerWidth = 360; // Official web UI uses 360px for widget drawer and 270px for main drawer
 export const drawerHeight = '60vh'; // For mobile mode
-export const drawerOffset = 400;
