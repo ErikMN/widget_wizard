f6be9d8db6f5758b997f2e89779d05cd1723d00c
ErikMN <yubnut@gmail.com>
2024-12-21 15:51:03 +0100

    Add videoplayer

---
+++ web/src/components/videoplayer/streams/components/helpers/sleep.ts
@@ -0,0 +1,10 @@
+/**
+ * Return a promise that resolves after a specific time.
+ * @param  ms Waiting time in milliseconds
+ * @return Resolves after waiting time
+ */
+export const sleep = async (ms: number) => {
+  return await new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
