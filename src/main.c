7dc6e71c37536fa2ca9aec9878184fd4f5abf5ab
ErikMN <yubnut@gmail.com>
2025-12-13 19:48:56 +0100

    Add WS support

--- src/main.c
+++ src/main.c
@@ -14,12 +14,69 @@
 #include <stdint.h>
 #include <math.h>
 #include <syslog.h>
+#include <signal.h>
+
+#include <libwebsockets.h>
 #include <glib/gstdio.h>
+#include <glib.h>
 #include <axsdk/axparameter.h>
 
 #define PARAM_NAME_APPLICATION_RUNNING_PARAM "ApplicationRunning"
+#define WS_PORT 9000
 
 static GMainLoop *main_loop = NULL;
+static struct lws_context *lws_ctx = NULL;
+
+static int
+ws_callback(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len)
+{
+  (void)wsi;
+  (void)user;
+  (void)in;
+
+  switch (reason) {
+
+  case LWS_CALLBACK_ESTABLISHED:
+    syslog(LOG_INFO, "WebSocket client connected");
+    break;
+
+  case LWS_CALLBACK_RECEIVE:
+    /* Log received data */
+    syslog(LOG_INFO, "WebSocket received %zu bytes", len);
+    break;
+
+  case LWS_CALLBACK_CLOSED:
+    syslog(LOG_INFO, "WebSocket client disconnected");
+    break;
+
+  default:
+    break;
+  }
+
+  return 0;
+}
+
+static const struct lws_protocols protocols[] = { {
+                                                      .name = "sysstats",
+                                                      .callback = ws_callback,
+                                                      .per_session_data_size = 0,
+                                                      .rx_buffer_size = 0,
+                                                      .id = 0,
+                                                      .user = NULL,
+                                                      .tx_packet_size = 0,
+                                                  },
+                                                  { NULL, NULL, 0, 0, 0, NULL, 0 } };
+
+static gboolean
+lws_glib_service(gpointer user_data)
+{
+  struct lws_context *context = user_data;
+
+  /* Non-blocking service */
+  lws_service(context, 0);
+
+  return G_SOURCE_CONTINUE;
+}
 
 static void
 handle_sigterm(int signo)
@@ -47,6 +104,7 @@ main(int argc, char **argv)
   AXParameter *parameter;
   GError *error = NULL;
   int ret = 0;
+  struct lws_context_creation_info info;
 
   init_signals();
 
@@ -56,7 +114,7 @@ main(int argc, char **argv)
   openlog(APP_NAME, LOG_PID | LOG_CONS, LOG_USER);
 
   /* Choose between { LOG_INFO, LOG_CRIT, LOG_WARN, LOG_ERR } */
-  syslog(LOG_INFO, "%s ACAP does not have any backend; entering idle state.", APP_NAME);
+  syslog(LOG_INFO, "%s starting WebSocket backend.", APP_NAME);
 
   /* Create AXParameter */
   parameter = ax_parameter_new(APP_NAME, &error);
@@ -71,8 +129,31 @@ main(int argc, char **argv)
     syslog(LOG_WARNING, "Failed to set parameter %s: %s", PARAM_NAME_APPLICATION_RUNNING_PARAM, error->message);
   }
 
+  /* Create WebSocket context */
+  memset(&info, 0, sizeof(info));
+  info.port = WS_PORT;
+  info.protocols = protocols;
+  info.gid = -1;
+  info.uid = -1;
+
+  lws_ctx = lws_create_context(&info);
+  if (!lws_ctx) {
+    syslog(LOG_ERR, "Failed to create libwebsockets context");
+    ret = -1;
+    goto exit;
+  }
+
+  /* Drive libwebsockets from GLib */
+  g_timeout_add(10, lws_glib_service, lws_ctx);
+
+  syslog(LOG_INFO, "WebSocket server listening on port %d", WS_PORT);
+
   /* Start the main loop */
   g_main_loop_run(main_loop);
+
+  /* Cleanup WebSocket context */
+  lws_context_destroy(lws_ctx);
+  lws_ctx = NULL;
 
   /* Unref the main loop */
   g_main_loop_unref(main_loop);
@@ -83,9 +164,11 @@ main(int argc, char **argv)
   }
 
 exit:
-  syslog(LOG_INFO, "Terminating %s dummy backend.", APP_NAME);
+  syslog(LOG_INFO, "Terminating %s backend.", APP_NAME);
 
-  ax_parameter_free(parameter);
+  if (parameter) {
+    ax_parameter_free(parameter);
+  }
 
   /* Close application logging to syslog */
   closelog();
