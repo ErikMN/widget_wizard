fc5d2b6614457926fa9ef075a438c5ab2652f9f9
ErikMN <yubnut@gmail.com>
2025-12-13 17:40:50 +0100

    start.sh: Don't source setuptarget.sh

--- web/start.sh
+++ web/start.sh
@@ -7,16 +7,20 @@ original_dir=$(pwd)
 project_root_dir="$(dirname "$0")"
 
 # Source version.sh:
+# shellcheck disable=SC1091
 . "${project_root_dir}/version.sh"
 
-# Source setuptarget.sh:
-. "${project_root_dir}/../setuptarget.sh"
+# Abort if credentials are not set:
+if [ -z "$TARGET_IP" ]; then
+  echo "Error: TARGET_IP is not set. Source setuptarget.sh first"
+  exit 1
+fi
 
 # Export environment variables required by React:
 export VITE_TARGET_IP=$TARGET_IP
 
 # Return to the original directory:
-cd "$original_dir"
+cd "$original_dir" || exit 1
 
 # Start the React application:
 vite
