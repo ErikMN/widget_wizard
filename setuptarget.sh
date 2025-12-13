7d2297f32af971203e049f488415e714056e0c81
ErikMN <yubnut@gmail.com>
2025-12-13 17:39:44 +0100

    setuptarget.sh: Make more robust

--- setuptarget.sh
+++ setuptarget.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 # SOURCE ME!
+#
+# Setup target device credentials and git hooks
+#
 
 # Print colors:
 FMT_RED=$(printf '\033[31m')
@@ -11,17 +13,20 @@ FMT_WHITE=$(printf '\033[37m')
 FMT_BOLD=$(printf '\033[1m')
 FMT_RESET=$(printf '\033[0m')
 
-# BASH: Print warning if script is not sourced.
-[[ "${BASH_SOURCE[0]}" != "${0}" ]] || echo "${FMT_RED}This script needs to be sourced, not run directly.${FMT_RESET}"
+# BASH: Print warning if script is not sourced then exit.
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  echo "${FMT_RED}This script needs to be sourced, not run directly.${FMT_RESET}"
+  exit 1
+fi
 
 # NOTE: need to source this script from the project dir.
-# Determine the script directory:
-SCRIPT_DIR="$(pwd)"
+# BASH: Determine the script directory:
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# HACK: trim web from end of path if sourcing from that dir:
+# BASH: HACK: trim web from end of path if sourcing from that dir:
 SCRIPT_DIR=${SCRIPT_DIR%/web}
 
-# Don't polute git status with modified .vscode stuff (disable: --no-skip-worktree)
+# HACK: Don't pollute git status with modified .vscode stuff (disable: --no-skip-worktree)
 # https://stackoverflow.com/questions/1274057/how-do-i-make-git-forget-about-a-file-that-was-tracked-but-is-now-in-gitignore
 # Safely mark .vscode files as skip-worktree (only if they are tracked)
 if [ -d "$SCRIPT_DIR/.vscode" ]; then
@@ -37,6 +42,7 @@ fi
 # Set the git hooks path:
 git config core.hooksPath "$SCRIPT_DIR/hooks"
 
+# Remove old .eap-install.cfg file if it exists:
 rm -f "${SCRIPT_DIR}/.eap-install.cfg"
 
 if ! command -v jq >/dev/null 2>&1; then
@@ -81,6 +87,7 @@ export TARGET_IP
 export TARGET_USR
 export TARGET_PWD
 
+# Read packagename from package.conf:
 package_conf_file="$SCRIPT_DIR/package.conf"
 packagename=$(grep '^PACKAGENAME=' "$package_conf_file" | awk -F'=' '{print $2}' | tr -d '"')
 
