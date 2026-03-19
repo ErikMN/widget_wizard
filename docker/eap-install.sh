36674806170e8ce91d7fa6d9e2c9beeb214c209f
ErikMN <yubnut@gmail.com>
2026-03-19 20:09:29 +0100

    eap-install.sh: Try to enable unsigned mode

--- docker/eap-install.sh
+++ docker/eap-install.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -e
 
 GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 NC='\033[0m'
 
 # Source the SDK:
 . /opt/axis/acapsdk/environment-setup*
 
-echo
 echo -e "${GREEN}>>> Installing using ACAPSDK $OECORE_SDK_VERSION for $OECORE_TARGET_ARCH${NC}"
-echo
 
 # NOTE: Make sure SDK eap-install.sh supports setting the HTTP port:
 export axis_device_ip=$TARGET_IP:$TARGET_PORT
 export password=$TARGET_PWD
+
+# Try to enable unsigned app mode on target:
+if ! curl --fail --silent --show-error --anyauth --noproxy "*" -u "${TARGET_USR}:${TARGET_PWD}" \
+  "http://${TARGET_IP}:${TARGET_PORT}/axis-cgi/applications/config.cgi?action=set&name=AllowUnsigned&value=true" >/dev/null; then
+  echo -e "${YELLOW}>>> Failed to enable unsigned mode${NC}"
+else
+  echo -e "${GREEN}>>> Unsigned mode enabled${NC}"
+fi
+echo
 
 eap-install.sh
